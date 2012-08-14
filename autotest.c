8a3014af2a227882d47abc5186b6dd7ec0d82408
Wolfgang Spraul <wspraul@q-ag.de>
2012-08-14 04:50:54 +0200

    planning some libs: model, bits, floorplan, control, test

---
+++ autotest.c
@@ -0,0 +1,24 @@
+//
+// Author: Wolfgang Spraul
+//
+// This is free and unencumbered software released into the public domain.
+// For details see the UNLICENSE file at the root of the source tree.
+//
+
+#include "model.h"
+#include "floorplan.h"
+#include "control.h"
+
+int main(int argc, char** argv)
+{
+	struct fpga_model model;
+	int rc;
+
+	if ((rc = fpga_build_model(&model, XC6SLX9_ROWS, XC6SLX9_COLUMNS,
+			XC6SLX9_LEFT_WIRING, XC6SLX9_RIGHT_WIRING)))
+		goto fail;
+
+	return EXIT_SUCCESS;
+fail:
+	return rc;
+}
---
+++ floorplan.h
@@ -0,0 +1,12 @@
+//
+// Author: Wolfgang Spraul
+//
+// This is free and unencumbered software released into the public domain.
+// For details see the UNLICENSE file at the root of the source tree.
+//
+
+int printf_tiles(struct fpga_model* model);
+int printf_devices(struct fpga_model* model);
+int printf_ports(struct fpga_model* model);
+int printf_conns(struct fpga_model* model);
+int printf_switches(struct fpga_model* model);
